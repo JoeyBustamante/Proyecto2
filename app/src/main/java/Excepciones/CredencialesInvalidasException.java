cd05f2d4e08f4f67c5d96fe5edfba46d7c7cf789
Joey Bustamante <jpbb2004@outlook.com>
2024-08-11 10:24:33 -0500

    Se crearon los metodos para iniciar secion con la excepcion correspondiente

---
+++ app/src/main/java/Excepciones/CredencialesInvalidasException.java
@@ -0,0 +1,8 @@
+package Excepciones;
+
+public class CredencialesInvalidasException extends  Exception {
+    public CredencialesInvalidasException(String msj){
+        super(msj);
+
+    }
+}
--- app/src/main/java/com/example/proyecto2/iniciarSecion.java
+++ app/src/main/java/com/example/proyecto2/iniciarSecion.java
@@ -1,12 +1,17 @@
 package com.example.proyecto2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import Excepciones.CredencialesInvalidasException;
 
 public class iniciarSecion extends AppCompatActivity {
 
@@ -15,10 +20,33 @@ public class iniciarSecion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_iniciar_secion);
+
+        EditText usuarioTxt = findViewById(R.id.usuarioTxt);
+        EditText contrasenaTxt = findViewById(R.id.contrasenaTxt);
+        Button loginBtn = findViewById(R.id.iniciarSecionBtn2);
+
+        loginBtn.setOnClickListener(v -> {
+            String usuario = usuarioTxt.getText().toString();
+            String contrasena = contrasenaTxt.getText().toString();
+
+            try {
+                autentificar(usuario, contrasena);
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
+            } catch (CredencialesInvalidasException w) {
+                Toast.makeText(this, w.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void autentificar(String usuario, String contrasena) throws CredencialesInvalidasException {
+        if (!"admin".equals(usuario) || !"1234".equals(contrasena)) {
+            throw new CredencialesInvalidasException("Usuario o contraseña incorrectos");
+        }
     }
 }
